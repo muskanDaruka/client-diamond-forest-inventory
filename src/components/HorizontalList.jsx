3a8eb11a0d413edfc8e1d25a0aec55f7e9c83ddb
muskanDaruka <darukamuskan@gmail.com>
2025-04-19 18:35:58 +0530

    fix: image size

--- src/components/HorizontalList.jsx
+++ src/components/HorizontalList.jsx
@@ -8,12 +8,12 @@ const HorizontalList = ({ data, setCurrentSlide, currentSlide }) => {
 
     return (
         <div className="flex items-center justify-center ">
-            <ul className="flex flex-wrap space-x-2 sm:space-x-4 mb-2 ">
+            <ul className="flex flex-wrap space-x-2 sm:space-x-4 mb-2">
                 {data.map((item, index) => (
                     <li
                         key={item}
                         onClick={() => handleItemClick(index)}
-                        className={`mb-2 sm:mb-0 sm:mr-2 text-center mx-auto text-white hover:text-[#666769] text-md xl:text-2xl cursor-pointer px-10 ${index === currentSlide ? "text-white font-bold text-md underline xl:text-2xl" : "text-md xl:text-2xl"}`}
+                        className={`mb-2 sm:mb-0 px-24 text-center mx-auto text-white hover:text-[#666769] text-md xl:text-2xl cursor-pointer border ${index === currentSlide ? "text-white font-bold text-md underline xl:text-2xl" : "text-md xl:text-2xl"}`}
                     >
                         {item}
                     </li>
