cd65f7bb026abe90120888b8d1df76273d4771cc
muskanDaruka <darukamuskan@gmail.com>
2025-04-21 17:54:34 +0530

    fix: css of list

--- src/components/HorizontalList.jsx
+++ src/components/HorizontalList.jsx
@@ -13,7 +13,7 @@ const HorizontalList = ({ data, setCurrentSlide, currentSlide }) => {
                     <li
                         key={item}
                         onClick={() => handleItemClick(index)}
-                        className={`mb-2 sm:mb-0 px-24 text-center mx-auto text-white hover:text-black hover:bg-white text-md xl:text-2xl cursor-pointer border ${index === currentSlide ? "text-white font-bold text-md underline xl:text-2xl" : "text-md xl:text-2xl"}`}
+                        className={`mb-2 sm:mb-0 px-24 text-center mx-auto text-white hover:text-black hover:bg-white text-md xl:text-2xl cursor-pointer border border-white ${index === currentSlide ? "text-white font-bold text-md underline xl:text-2xl" : "text-md xl:text-2xl"}`}
                     >
                         {item}
                     </li>
